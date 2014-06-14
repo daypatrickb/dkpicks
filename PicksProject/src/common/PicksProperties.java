ae5c21be10df802d7c64ede695dce5e4fc81fc18
dhall622 <dehall622@gmail.com>
2014-06-14 11:12:51 -0700

    Add properties file

---
+++ PicksProject/src/common/PicksProperties.java
@@ -0,0 +1,31 @@
+package common;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PicksProperties {
+
+	private static Properties prop;
+	
+	static
+	{
+		try 
+		{
+			prop = new Properties();
+			ClassLoader loader = Thread.currentThread().getContextClassLoader();           
+			InputStream stream = loader.getResourceAsStream("/picks.properties");
+			prop.load(stream);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static String get(String key)
+	{
+		return prop.getProperty(key);
+	}
+	
+	
+}
